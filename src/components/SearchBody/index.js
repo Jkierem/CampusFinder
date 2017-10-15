5dde76c998f75e608e857e5ff71afab7b6dad17c
Jkierem <juanmig8@hotmail.com>
2017-10-15 11:28:50 -0500

    Added express-server

--- src/components/SearchBody/index.js
+++ src/components/SearchBody/index.js
@@ -1,13 +1,59 @@
 import React from 'react'
+import { Grid , Button, Form, Dropdown } from 'semantic-ui-react'
+import { buildings } from '../../resources/Buildings'
 
 class SearchBody extends React.Component{
 	constructor(props){
 		super(props);
-		this.state={}
+		this.state={
+			loading: false
+		}
+	}
+
+	handleChange = (e, { name, value }) => {
+		this.setState({ [name]: value })
+	}
+
+	onSearch = () => {
+		this.setState({
+			loading: !this.state.loading
+		})
+		window.setTimeout(()=>{
+			this.setState({
+				loading: !this.state.loading
+			})
+		},1000)
+
 	}
 
 	render(){
-		return(<div/>);
+		return(
+			<Form>
+				<Grid padded centered columns={16}>
+					<Grid.Row centered>
+						<Grid.Column textAlign='center' width={14} style={{maxWidth:350}}>
+							<Form.Field>
+								<Dropdown
+									placeholder={"Buscar..."}
+									onChange={this.handleChange}
+									name={"edificio"}
+									search
+									selection
+									options={buildings}
+								/>
+							</Form.Field>
+						</Grid.Column>
+					</Grid.Row>
+					<Grid.Row centered>
+						<Grid.Column textAlign='center' width={14} style={{maxWidth:350}}>
+							<Form.Field>
+								<Button content={"Buscar"} onClick={this.onSearch} />
+							</Form.Field>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Form>
+		);
 	}
 }
 
